a04a24ada07c8f149bb2d259b9784fbf12ba74cc
Roberti Yavier Amarante Garcia <yavier.ag@gmail.com>
2016-10-17 18:22:02 -0400

    Parcial 1-Aplicada1

--- Parcial1-Aplicada1-RobertiAmarante/FormularioPrincipal.cs
+++ Parcial1-Aplicada1-RobertiAmarante/FormularioPrincipal.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace Parcial1_Aplicada1_RobertiAmarante
 {
-    public partial class Form1 : Form
+    public partial class FormularioPrincipal : Form
     {
-        public Form1()
+        public FormularioPrincipal()
         {
             InitializeComponent();
         }
